d573fbb582e3317d46274f536238dc64f324b3f2
Prof. Mozekson <vkoudelka@gmail.com>
2014-10-07 10:41:22 -0400

    New comments and .gitignore.

--- hello.c
+++ hello.c
@@ -1,7 +1,26 @@
+/* 
+ * Nazdar 
+ *
+ * Ja Sam
+ * 07.10.2014 
+ *  Tohle je pokus.
+ *  See README for more details.
+ */
+
 #include <stdio.h>
 
+/*
+ * This is main function
+ */
 int main(int argc, char **argv) {
+	/*
+	 * Initialize.
+	 */
 	printf("Vole\n");
+
+	/* Return. */
 	return 1;
+
+	/* unreachable */
 }
 
