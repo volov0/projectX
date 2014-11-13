ae9cc702585938b5002aad831f42f365a92e638e
Prof. Mozekson <vkoudelka@gmail.com>
2014-11-13 10:43:46 -0500

    aa

--- stanford/l03/structs.cpp
+++ stanford/l03/structs.cpp
@@ -9,20 +9,29 @@
 #include <iostream>
 #include <iomanip>
 
+#define FRACTION_PRINT(f) f.num << "/" << f.denum
+
 using namespace std;
 
 struct fraction {
 	int num;
 	int denum;
-}
+};
 
+/**
+ * Main function
+ */
 int main(int argc, char **argv) {
-	fraction pi;
+	struct fraction pi;
 
 	pi.num = 22;
 	pi.denum = 7;
 
-	cout << "running" << endl;
+	cout << "pi " << FRACTION_PRINT(pi) << endl;
+
+	((fraction *)&(pi.denum))->num = 12;         //to je ale prasarna, menim pi.denum!
+	((fraction *)&(pi.denum))->denum = 33;       //jeste hur, prepisuju pamet za pi... buh vi co tam je
+	cout << "pi " << FRACTION_PRINT(pi) << endl;
 
 	return 0;
 }
