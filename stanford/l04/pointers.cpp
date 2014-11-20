b7392c4b511ed564f8a9d4bbbe254ceeb5bc9a56
Prof. Mozekson <vkoudelka@gmail.com>
2014-11-20 10:59:16 -0500

    a

--- stanford/l04/pointers.cpp
+++ stanford/l04/pointers.cpp
@@ -32,8 +32,31 @@ int main(int argc, char **argv) {
 	cout << "x = " << x << ", y = " << y << endl;
 
 	short s = 4;
-	swap(&x,&s, sizeof(int));   // noahodou funguje
+	swap(&x,&s, sizeof(int));   
 	cout << "x = " << x << ", s = " << s << endl;
+
+	/* test 02 
+	 * !!!!Pozor tohle je dulezite!!!!
+	 * - Kdyz swapuju inty, swap predam jako parametry adresy intu - int *
+	 * - Kdyz swapuju stringy (char *), musim swapu predat adresu, kde je adresa stringu
+	 *   swap vubec nevi o tom, ze tam jsou nejake stringy prehodi pouze adresy,
+	 *   ktere ukazuji nekam na heap.
+	 *   Takze swap dostane adresu adresy - char **
+	 */
+	cout << HEADER("TEST 02") << endl;
+	char *husband = strdup("Pepa");
+	char *wife    = strdup("Brunhilda");
+	cout << "husband: " << husband << ", wife: "<< wife << endl;
+	cout << "&husband: " << &husband << endl;
+	cout << "&wife:    " << &wife << endl;
+	cout << "&&husband: " << (int *)husband << endl;
+	cout << "wife:    " << wife << endl;
+	/* udelam swap */
+	cout << "swap(&husband,&wife,sizeof(char *))" << endl;
+	swap(&husband, &wife, sizeof(char *));
+	cout << "husband: " << husband << ", wife: "<< wife << endl;
+	cout << "&husband: " << &husband << endl;
+	cout << "&wife:    " << &wife << endl;
 
 	return 0;
 }
@@ -58,6 +81,8 @@ void swap_int(int *ap, int *bp) {
  * @param vp1 pointer na prvni vec
  * @param vp2 pointer na druhou vec
  * @param size velikost dat k prehozeni
+ *             muze se dat const, ale const se obecne pouziva
+ *             pri sdileni dat 
  */
 void swap(void *vp1, void *vp2, int size) {
 	char buffer[size];   // je mozne urcit velikost pole z parametru funkce v g++
@@ -72,9 +97,12 @@ void swap(void *vp1, void *vp2, int size) {
  * Function: swap_my
  * -----------------
  * Genericky swap... prehodi dve jakekoliv veci po bitech, moje verze
+ *                   je tu problem, ze kopiruje od konce...
  * @param vp1 pointer na prvni vec
  * @param vp2 pointer na druhou vec
  * @param size velikost dat k prehozeni
+ *             muze se dat const, ale const se obecne pouziva
+ *             pri sdileni dat 
  */
 void swap_my(void *vp1, void *vp2, int size) {
 	char buffer;     
