d3b6b448b72fddf94c8e936516b3aaa1df0f0a57
volov0 <v.koudelka@gmail.com>
2014-11-23 17:43:47 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,19 +1,21 @@
-/* 
- * Nazdar 
- *
- * Ja Sam
- * 07.10.2014 
- *  Tohle je pokus.
- *  See README for more details.
+/** 
+ * @file   hello.c
+ * @author Ja Sam
+ * @date   07.10.2014 
+ * @brief  Nazdar 
+ * 
+ * Tohle je pokus.
+ * See README for more details.
  */
 
 #include <stdio.h>
 
-/*
- * This is main function
+/**
+ * Function main
+ * -------------
  */
 int main(int argc, char **argv) {
-	/*
+	/**
 	 * Initialize.
 	 */
 	printf("Vole\n");
