4ef4171e482e5e3155d401596c131f1ceae8b0e6
Profesor Mozekson <v.koudelka@gmail.com>
2014-12-18 20:04:38 +0100

    a

---
+++ stanford/l05/cpp_search.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file   cpp_search.cpp
+ * @author volovo
+ * @date   18.12.2014  
+ * @brief  Template search 
+ * @detail Sample na genericke vyhledavani v poli libovolnych struktur.
+ *         Pouziva template.
+ */
+
+#include <iostream>
+#include "tsearch.hpp"
+
+using namespace std;
+
+/**
+ * Function main
+ * -------------
+ * - test 01 - generic search v poli intu
+ * - test 02 - generic search v poli stringu
+ */
+int main(int argc, char **argv) {
+	/* test 1 -   */
+	cout << " --- TEST 01 --- " << endl;
+	int arr[10] = {78, 47, 32, 40, 7, 55, 96, 1, 72, 20 };
+
+ 	return 0;
+}
+
---
+++ stanford/l05/tsearch.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file   tsearch.hpp
+ * @author volovo
+ * @date   18.12.2014  
+ * @brief  Template search 
+ * @detail Genericke vyhledavani v poli libovolnych struktur.
+ */
+
+#ifndef TSEARCH_HPP
+#define TSEARCH_HPP
+
+/**
+ * Template function tsearch
+ * -------------------------
+ */
+template <class T>
+int tsearch(T *key, T *arr, int n);
+
+#endif  /* TSEARCH_HPP */
