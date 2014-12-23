a80ed8395a9f79fa300783c5478e9b8b8302d482
Prof. Mozekson <vkoudelka@gmail.com>
2014-12-23 08:43:11 -0500

    a

---
+++ stanford/headers/mydefs.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file   mydefs.hpp
+ * @author Vaclav Koudelka
+ * @date   20.11.2014
+ * @brief  Common header file with basic definitions.
+ *
+ */
+
+#ifndef MYDEFS_HPP
+#define MYDEFS_hPP
+
+
+#ifndef HEADER
+  #define HEADER(t) "-------------------\n----  " << t << "  ----\n-------------------"
+#endif
+
+
+#endif  /* MYDEFS_HPP */
