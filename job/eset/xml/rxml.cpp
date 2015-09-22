e934732fdf45daa30e38ad6392ce088e8074d751
unknown <kouva01@KOUVA01.ca.com>
2015-09-22 16:34:29 +0200

    First window commit

--- job/eset/xml/rxml.cpp
+++ job/eset/xml/rxml.cpp
@@ -10,159 +10,168 @@
 #include <regex>             
 #include "taqueue.hpp"       
 
-using namespace std;
-
-void reader_writer(ifstream& file, asynch_queue<char>& inq, asynch_queue<char>& outq);
-void tag_filter(asynch_queue<char>& inq, asynch_queue<char>& outq); 
-void printer(asynch_queue<char>& q);
-bool start_of_prohibited_tag(const std::string& s, std::string& tag); 
-bool end_of_prohibited_tag(const std::string& s, const std::string& tag); 
-
-
+void reader_writer(std::ifstream& file, asynch_queue<char>& inq, asynch_queue<char>& outq);
+void tag_filter(asynch_queue<char>& inq, asynch_queue<char>& outq);
+bool start_of_prohibited_tag(const std::string& s, std::string& tag);
+bool end_of_prohibited_tag(const std::string& s, const std::string& tag);
 
 /**
- * Function main
- * -------------
+ * Opens file and runs thread to process file.
  */
 int main(int argc, char **argv) {
-	ifstream myfile;
+	std::ifstream myfile;
 	asynch_queue<char> inbuffer;
 	asynch_queue<char> outbuffer;
 
-	myfile.open("index.xml");
-	if (!myfile.is_open()) {
-		cout << "Can't open file." << endl;
-		exit(0);
+	/* read argument */
+	if (argc == 1) {
+		std::cerr << "You need to specify file." << std::endl;
+		return 1;
 	}
 
-	thread t1(reader_writer, ref(myfile), ref(inbuffer), ref(outbuffer));
-	thread t2(tag_filter, ref(inbuffer), ref(outbuffer));
-	// todo: join xml filter
+	/* open input file */
+	myfile.open(argv[1]);
+	if (!myfile.is_open()) {
+		std::cout << "Can't open file." << std::endl;
+		return 2;
+	}
+
+	/* init working threads */
+	std::thread t1(reader_writer, std::ref(myfile), std::ref(inbuffer), std::ref(outbuffer));
+	std::thread t2(tag_filter, std::ref(inbuffer), std::ref(outbuffer));
+	//std::thread t3(printer, std::ref(outbuffer));
+
+	/* collect threads */
 	t1.join();
 	t2.join();
-	//thread t3(printer, ref(outbuffer));
+	//t3.join();
 
 	myfile.close();
-
 
 	return 0;
 }
 
 /**
- * Function reader_writer
- * ----------------------
+ * Reads input queue and places filtered text into ouptut queue
+ * @param file opened file with xml input
+ * @param inq input queue to be populated with xml from input file
+ * @param outq output queue containg filtered xml which needs to be printed
  */
-void reader_writer(ifstream& file, asynch_queue<char>& inq, asynch_queue<char>& outq) {
+void reader_writer(std::ifstream& file, asynch_queue<char>& inq, asynch_queue<char>& outq) {
 	char c;
-	this_thread::sleep_for(chrono::milliseconds(10));
 
 	while (file.get(c)) {
 		/* add to input buffer */
 		inq.push(c);
 
-		//this_thread::sleep_for(chrono::milliseconds(12));
-
 		/* try read from output buffer */
 		while (outq.try_pop(c)) {
-			cout << c;
+			std::cout << c;
 		}
 	}
 
-	// say file is processed
+	/* say file is processed */
 	inq.terminate();
 
 	/* read last chunk from output buffer */
-	cout << "rw flush outq" << endl;
 	while (outq.pop(c)) {
-		cout << c;
+		std::cout << c;
 	}
-	cout << "rw flush DONE" << endl;
 }
 
 /**
- * Function tag_filter
- * -------------------
- */
+ * Reads input queue and places filtered text into ouptut queue
+ * @param inq input queue containg xml
+ * @param outq output queue containg filtered xml
+*/
 void tag_filter(asynch_queue<char>& inq, asynch_queue<char>& outq) {
-	char c; 
-	bool in_tag = false, in_proh = false;
-	string buffer, start_tag;
+	char c;
+	bool in_tag = false;       // says that processing is inside <,> brackets
+	bool in_proh = false;      // says that processing is inside of text which should not be printed
+	std::string buffer;        // working buffer - usually contains tag which is currently processed
+	std::string start_tag;     // if processing is "in_proh" state it contains name of prohibited tag
 
-	while(inq.pop(c)) { 
-		//this_thread::sleep_for(chrono::milliseconds(10));
-
-		/* process character */
-
+	/* process characters from queue one by one */
+	while (inq.pop(c)) {
 		if (in_tag) {
 			buffer += c;
 			if (c == '>') {
-				// process buffer
+				/* process buffer - it contains complete tag at this point */
 				if (in_proh) {
 					if (end_of_prohibited_tag(buffer, start_tag)) {
+						/* say we are out of "prohibited" mode - means output can be printed */
 						in_proh = false;
 					}
 				}
-				//cout << "tw >>>> " << buffer << endl;
 				else if (start_of_prohibited_tag(buffer, start_tag)) {
-					if (buffer[buffer.size() - 2] != '/') {   // single terminated tag
+					/* check for single tag */
+					if (buffer[buffer.size() - 2] != '/') {
+						/* say we are in "prohibited" mode - means output is not printed */
 						in_proh = true;
-						cerr << "tw start of proh " << buffer << endl;
 					}
 				}
 				else {
+					/* buffer allowed tag so print it */
 					for (unsigned i = 0; i < buffer.size(); i++)
-						outq.push(buffer[i]); 
+						outq.push(buffer[i]);
 				}
+				/* clear buffer for next tag */
 				buffer.clear();
 				in_tag = false;
 			}
 		}
 		else {
-			/* put result to the output buffer */
 			if (c == '<') {
+				/* tag start encountered - let's put it to buffer to see if it is allowed */
 				in_tag = true;
 				buffer += c;
 			}
 			else if (!in_proh) {
+				/* c is simple character - put it to the output buffer */
 				outq.push(c);
 			}
 		}
-
 	}
-	
+
 	/* say filtering is done */
-	cout << "tw terminate" << endl;
 	outq.terminate();
 }
 
-bool end_of_prohibited_tag(const std::string& s, const std::string& tag) {
-	std::string cs;
-	cs = "</>" + tag + ">";
-	cerr << "EOPT testing " << s << " tag: " << tag << "res: " << (s == cs) << endl;
+/**
+ * Determines if tag is terminate tag of "prohibited" tag
+ * @param st which contains complete content of parsed tag including <,> brackets
+ * @param tag_name contains name of "prohibited" tag
+ * @return true if input tag is found to be terminating "prohibited" tag
+ */
+bool end_of_prohibited_tag(const std::string& s, const std::string& tag_name) {
+	std::string cs("</" + tag_name + ">");
 	return (s == cs);
 }
-bool start_of_prohibited_tag(const std::string& s, std::string& tag) {
+
+/**
+* Determines if tag is "prohibited"
+* @param st which contains complete content of parsed tag including <,> brackets
+* @param tag_name if returns true then simple name of prohibited tag is set
+* @return true if input tag is found prohibited
+*/
+bool start_of_prohibited_tag(const std::string& st, std::string& tag_name) {
 	std::smatch sm;
-	std::regex e("<(img|object|script).*>");   // buggy in g++
-	cerr << "SOPT testing " << s;
-	if (std::regex_match(s, sm, e)) {
-		tag = sm[1];
-		cerr << " TRUE - " << sm[1] << endl;
+	std::regex e("<(img|object|script).*>");   // buggy in gcc
+	if (std::regex_match(st, sm, e)) {
+		tag_name = sm[1];
 		return true;
 	}
 	else {
-		cerr << " FALSE" << endl;
 		return false;
 	}
 }
 
 /**
- * Function printer
- * ----------------
+ * printer
  */
 void printer(asynch_queue<char>& q) {
 	char c;
 	while (q.pop(c)) {
-		cout  << c;
+		std::cout << c;	
 	}
 }
--- job/eset/xml/taqueue.hpp
+++ job/eset/xml/taqueue.hpp
@@ -9,7 +9,6 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
-using namespace std;
 
 /**
  * Template for queue with concurrent access and mechanism to handle
@@ -19,9 +18,9 @@ template<typename T>
 class asynch_queue
 {
 private:
-	queue<T> q; 
-	mutex cv_m;            
-	condition_variable cv;
+	std::queue<T> q;
+	std::mutex cv_m;
+	std::condition_variable cv;
 	bool processing_done;
 public:
 	/**
@@ -34,29 +33,22 @@ public:
 	 * @param v value to be pushed to a queue
 	 */
 	void push(T const& v) {
-		unique_lock<mutex> lk(cv_m);
+		std::unique_lock<std::mutex> lk(cv_m);
 		q.push(v);
-		//lk.unlock();
+		lk.unlock();
 		cv.notify_one();
 	}
-	
-	/*void push(vector<T> const& vect) {
-		unique_lock<mutex> lk(cv_m);
-		for (int i = 0; i < vect.size(); i++)
-			q.push(vect[i]);
-		//lk.unlock();
-		cv.notify_one();
-	}*/
+
 
 	/**
-	 * Pop element from a queue. 
-	 * If queue is empty then wait until new element is pushed or
-	 * until queue is terminated.
-	 * @param v after return will contain poped element
-	 * @return true if element was poped, false if queue is empty and terminated
-	 */
-	bool pop (T& v) {
-		unique_lock<mutex> lk(cv_m);
+  	  * Pop element from a queue.
+	  * If queue is empty then wait until new element is pushed or
+	  * until queue is terminated.
+	  * @param v after return will contain poped element
+	  * @return true if element was poped, false if queue is empty and terminated
+	  */
+	bool pop(T& v) {
+		std::unique_lock<std::mutex> lk(cv_m);
 		while (q.empty() && !processing_done) {    // avoid spurious wakeup
 			cv.wait(lk);
 		}
@@ -66,24 +58,24 @@ public:
 
 		v = q.front();
 		q.pop();
-		//lk.unlock();
+		lk.unlock();
 		return true;
 	}
 
 	/**
-	 * Try to pop element from a queue. 
+	 * Try to pop element from a queue.
 	 * If queue is empty simply return false.
 	 * @param v after return will contain poped element
 	 * @return true if element was poped, false if queue is empty
 	 */
-	bool try_pop (T& v) {
-		unique_lock<mutex> lk(cv_m);
+	bool try_pop(T& v) {
+		std::unique_lock<std::mutex> lk(cv_m);
 		if (q.empty())
 			return false;
 
 		v = q.front();
 		q.pop();
-		//lk.unlock();
+		lk.unlock();
 		return true;
 	}
 
@@ -92,9 +84,9 @@ public:
 	 * Simply put after terminate call it is not allow to push anymore.
 	 */
 	void terminate() {
-		unique_lock<mutex> lk(cv_m);
+		std::unique_lock<std::mutex> lk(cv_m);
 		processing_done = true;
-		//lk.unlock();
+		lk.unlock();
 		cv.notify_one();
 	}
 };
