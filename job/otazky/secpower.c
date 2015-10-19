df49557e319b010e8b330f9edaab5c9e9ef26e75
Prof. Mozekson <vkoudelka@gmail.com>
2015-10-19 13:01:37 -0400

    secpower added

---
+++ job/otazky/secpower.c
@@ -0,0 +1,32 @@
+/**
+ * @file secpower.c
+ * @author Vaclav Koudelka
+ * @date 19.10.2015
+ * @brief funkce ktera zjisti zda csilo na vstupu je mocnina dvou
+ */
+
+#include <stdio.h>
+
+/**
+ * @param n input number
+ * @return 1 if input number n is power of two, 0 otherwise
+ */
+int secpower(int n) {
+	int i = n;
+	while(1) {
+		if (i & 0x1) return 0;
+		if (i == 2) return 1;
+		i = i >> 1;
+	}
+}
+
+int main(int argc, char **argv) {
+	int i = 612;
+
+	if (secpower(i)) 
+		printf("Number %d is power of two.\n", i);
+	else
+		printf("Number %d is not power of two.\n", i);
+	
+	return 0;
+}
